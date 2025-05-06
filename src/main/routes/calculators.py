7c13b0aa45b8ae0232053125a6ee327e5a0bc4b6
Felipe Andrade <felipes.andrade@gmail.com>
2025-05-05 23:32:48 -0300

    Updating calculators.py

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
-from src.main.factories import calculator1_factory
-from src.main.factories import calculator2_factory
+from src.main.factories.calculator1_factory import calculator1_factory
+from src.main.factories.calculator2_factory import calculator2_factory
 
 # Calculators routes
 # Naming calculators routes
@@ -8,11 +8,14 @@ calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    response = calculator1_factory.calculate(request)
+    calc = calculator1_factory()
+    response = calc.calculate(request)
+
     return jsonify(response), 200
 
 @calc_route_bp.route("/calculator/2", methods=["POST"])
 def calculator_2():
     calc = calculator2_factory()
     response = calc.calculate(request)
+
     return jsonify(response), 200
