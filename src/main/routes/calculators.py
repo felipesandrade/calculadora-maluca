671ebef6c90e4ba82be8bf929f88f380ced030e7
Felipe Andrade <felipes.andrade@gmail.com>
2025-05-06 23:31:31 -0300

    Implementing calculator 3

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from src.main.factories.calculator1_factory import calculator1_factory
 from src.main.factories.calculator2_factory import calculator2_factory
+from src.main.factories.calculator3_factory import calculator3_factory
 
 # Calculators routes
 # Naming calculators routes
@@ -16,6 +17,13 @@ def calculator_1():
 @calc_route_bp.route("/calculator/2", methods=["POST"])
 def calculator_2():
     calc = calculator2_factory()
+    response = calc.calculate(request)
+
+    return jsonify(response), 200
+
+@calc_route_bp.route("/calculator/3", methods=["POST"])
+def calculator_3():
+    calc = calculator3_factory()
     response = calc.calculate(request)
 
     return jsonify(response), 200
